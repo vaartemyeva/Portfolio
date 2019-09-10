fe9afc54d1a431968c74df7b2ab413396cd94250
belokon.e <myweb.joker@gmail.com>
2019-09-10 22:19:03 +0300

    Добавление скриптов

--- js/script.js
+++ js/script.js
@@ -1,10 +1,69 @@
-$(function(){
-  $('.menuToggle').on('click', function(){
-    $('.menu').slideToggle(300);
-  });
-});
-function(){
-  if($(this).css('display')==='none')
-  $(this).removeAltr('style';}
-});
+$(function () {
+  $("a[href^='#']").click(function () {
+  
+    var _href = $(this).attr("href");
+    
+    $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+    
+    return false;
+  
+  })
 
+  var nav = $("#main_menu").offset().top;
+
+  $(window).scroll(function () {
+
+    if ($(this).scrollTop() > nav) {
+
+    $("#main_menu").addClass('fixed');
+
+    } else {
+
+    $("#main_menu").removeClass('fixed');
+
+    }
+  });
+
+  // Перепиши на Jquery блеать!
+  var btn = document.getElementsByClassName("btn");
+
+btn = btn[2];
+
+console.log(btn);
+
+  var a = 2;
+
+  function giveMeAdvice(a) {
+
+    if (a == 1) {
+    
+    alert("Идем в магаз")
+    
+    } else if(a == 2) {
+    
+    alert("Идем домой");
+    
+    } else {
+    
+    alert("Ну и ладно!");
+    
+    }
+    
+    }
+    
+    
+    
+    btn.onclick = function (event) {
+    
+      event.preventDefault();
+    
+      giveMeAdvice(a);
+    
+    }
+
+    $(".owl-carousel").owlCarousel({
+      items: 1,
+      loop: 1
+    });
+
+});
